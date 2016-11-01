975fd816dac7c7dddf79662a1322290c1352359e
Abdelrhman Talat <abdelrhman@arabhosters.com>
2016-11-01 20:39:01 +0200

    grouping

--- src/main/java/chapter4/Playground.java
+++ src/main/java/chapter4/Playground.java
@@ -1,11 +1,12 @@
 package chapter4;
 
-import io.reactivex.Flowable;
 import io.reactivex.Observable;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import static chapter3.Playground.subscribePrint;
@@ -21,10 +22,40 @@ public class Playground {
 //                        .map(v -> v * 3)
 //                        .map(v -> (v % 2 == 0) ? "even" : "odd"), "Map: ");
 
-        subscribePrint(
-                listFolder(Paths.get("src", "main", "resources"), "{lorem.txt,letters.txt}")
-                        .flatMap(path -> from(path)), "FS"
-        );
+//        subscribePrint(
+//                listFolder(Paths.get("src", "main", "resources"), "{lorem.txt,letters.txt}")
+//                        .flatMap(path -> from(path)), "FS"
+//        );
+
+//        subscribePrint(
+//        Observable.just(-1,0,1)
+//                .map(v -> 2/v )
+//                .flatMap((v) -> Observable.just(v),
+//                        e -> Observable.just(0),
+//                        () -> Observable.just(42)),
+//                "flatMap");
+
+//        subscribePrint(
+//                Observable.just(5,411)
+//                .flatMap(v ->Observable.range(v, 2),
+//                        (x,y) -> x+y),
+//                "Combine"
+//        );
+
+        List<String> albums = Arrays.asList("The Piper at the Gates of Dawn",
+                "A Saucerful of Secrets",
+                "More", "Ummagumma", "Atom Heart Mother",
+                "Meddle", "Obscured by Clouds",
+                "The Dark Side of the Moon",
+                "Wish You Were Here", "Animals", "The Wall");
+//        Observable.fromIterable(albums)
+//                .groupBy(album -> album.split(" ").length)
+//                .subscribe(obs -> subscribePrint(obs, obs.getKey()+" Words."));
+
+        Observable.fromIterable(albums)
+                .groupBy(album -> album.replaceAll("[^mM]", "").length(), album -> album.replaceAll("[mM]", "*"))
+                .subscribe( obs -> subscribePrint(obs, obs.getKey()+" by occurrences of (m)"));
+
 
     }
 
